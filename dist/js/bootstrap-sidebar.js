0ec222e4fcd31f4e7847a7ed4f41d1b2c7134d6b
gozoro <gozoro@yandex.ru>
2019-06-06 20:23:53 +0300

    Preparing for a new version

--- dist/js/bootstrap-sidebar.js
+++ dist/js/bootstrap-sidebar.js
@@ -1,79 +1,14 @@
 /*
 * Simple responsive sidebar for Bootstrap 3
 * Author: Gozoro <gozoro@yandex.ru>
-* Version: 0.0.4
+* Version: 1.0.0
 *
 */
 
 (function()
 {
 
-	var scrollPos;
 
-	function sidebar_show()
-	{
-		$('body').addClass('modal-open');
-		$('body').append('<div class="modal-backdrop fade in"></div>');
-
-		$('.sidebar')
-			.addClass('open')
-			.animate({'left':'0'}, 300);
-
-		if(navigator.userAgent.match(/iPhone|iPad|iPod/i))
-		{
-			scrollPos = $('body').scrollTop();
-			$('body').css({position: 'fixed',top: -scrollPos});
-		}
-	}
-
-
-	function sidebar_hide()
-	{
-		$('.modal-backdrop').remove();
-		$('.sidebar')
-			.animate({'left':'-9999px'}, 300, function()
-			{
-
-				$('body').removeClass('modal-open');
-				$('.sidebar').removeClass('open');
-				$('.sidebar').removeAttr('style');
-
-				if(navigator.userAgent.match(/iPhone|iPad|iPod/i))
-				{
-					$('body').css('position', 'static').scrollTop(scrollPos);
-				}
-			});
-	}
-
-	$(document).ready(function()
-	{
-
-		$('.sidebar-open').click(function()
-		{
-			sidebar_show();
-		});
-
-
-
-		$('body').on('click', '.modal-backdrop', function()
-		{
-			if($('.sidebar').hasClass('open'))
-			{
-				sidebar_hide();
-			}
-		});
-
-
-
-		$('.sidebar-close').click(function()
-		{
-			if($('.sidebar').hasClass('open'))
-			{
-				sidebar_hide();
-			}
-		});
-
-	});
 
 })();
 
